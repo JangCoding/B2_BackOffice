9a83421df39e3a44586c7d73022e818f001004aa
codingjang <jjangga813@naver.com>
2024-01-22 15:02:09 +0900

    create project

---
+++ src/test/kotlin/com/b2_backoffice/b2_backoffice/B2BackofficeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.b2_backoffice.b2_backoffice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class B2BackofficeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
